93b57140b2483f9c7e73548e6db8651927420fd0
Shiksha1501 <shikshavishwakarma1501@gmail.com>
2024-03-09 12:39:26 +0530

    added homepage background

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 
 const Homepage = () => {
-  return <div>Home</div>;
+  return (
+    <Container
+      className="d-flex justify-content-center"
+      style={{ maxWidth: "1140px" }}
+    ></Container>
+  );
 };
 
 export default Homepage;
